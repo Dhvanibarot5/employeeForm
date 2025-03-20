ed85d28d6419cd61aff620cf306f3ade100d1137
Dhvanibarot5 <dhvanibarot5@gmail.com>
2025-03-20 17:51:38 +0530

    table structure done

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,62 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div>
+      <div className="container mx-auto text-center flex flex-col items-center">
+        <h1 className="text-3xl font-bold py-5">Employee Form</h1>
+        <table className="border border-black">
+          <tr>
+            <td>Name</td>
+            <td>
+              <input type="text" name="name" placeholder="Enter your name" />
+            </td>
+          </tr>
+          <tr>
+            <td>Email</td>
+            <td>
+              <input type="email" name="email" placeholder="Enter Email" />
+            </td>
+          </tr>
+          <tr>
+            <td>Password</td>
+            <td>
+              <input type="password" name="password" placeholder="Enter password" />
+            </td>
+          </tr>
+          <tr>
+            <td>Gender</td>
+            <td>
+              <input type="radio" /> Male
+              <input type="radio" className="ms-2" /> Female
+            </td>
+          </tr>
+          <tr>
+            <td>Hobby</td>
+            <td>
+              <input type="checkbox" /> Coding
+              <input type="checkbox" className="ms-2" /> Badminton
+              <input type="checkbox" className="ms-2" /> Cricket
+            </td>
+          </tr>
+        </table>
+
+        <h2 className="text-2xl font-medium">Data</h2>
+        <table>
+          <tr className="gap-3 flex">
+            <th>Name</th>
+            <th>Email</th>
+            <th>Gender</th>
+            <th>Hobby</th>
+          </tr>
+          <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+          </tr>
+        </table>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
